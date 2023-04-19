219a5c7d7763d6241a11b8bad146a3e0d290bbce
COWELL\haipt7478 <haipt7478@co-well.vn>
2023-04-19 09:26:53 +0700

    add loading to article web view

--- app/src/main/java/com/jaroid/newskt/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/jaroid/newskt/ui/fragments/ArticleFragment.kt
@@ -1,7 +1,9 @@
 package com.jaroid.newskt.ui.fragments
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
@@ -21,11 +23,23 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         binding = FragmentArticleBinding.bind(view)
 
         var article: Article?
+
         arguments?.let { it ->
             article = it.getSerializable("article") as Article?
             article?.let {
+                val client = object : WebViewClient() {
+                    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                        super.onPageStarted(view, url, favicon)
+                        binding.llLoading.visibility = View.VISIBLE
+                    }
+
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        super.onPageFinished(view, url)
+                        binding.llLoading.visibility = View.GONE
+                    }
+                }
                 binding.webView.apply {
-                    webViewClient = WebViewClient()
+                    webViewClient = client
                     loadUrl(it.url)
                 }
             }
